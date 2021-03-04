6a9cdb93eca1636d9481c9423d87548afdefca97
Simon Courtecuisse <simoncourtecuisse@gmail.com>
2021-03-04 11:19:10 +0100

    AgeCalculator

--- src/main/java/com/openclassrooms/SafetyNetAlerts/model/MedicalRecord.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/model/MedicalRecord.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.SafetyNetAlerts.model;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Date;
 
 
@@ -48,5 +50,14 @@ public class MedicalRecord {
 		this.allergies = allergies;
 	}
 
+	public class AgeCalculator{
+		public int calculateAge(LocalDate birthDate, LocalDate currentDate) {
+			if ((birthDate != null) && (currentDate != null)) {
+				return Period.between(birthDate, currentDate).getYears();
+			} else {
+				return 0;
+			}
+		}
+	}
 	
 }
